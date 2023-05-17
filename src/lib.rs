f5c73f85faeb3d7613687d30a12099b01bfa1938
Elizar Pepino <jupenz@gmail.com>
2023-05-17 18:52:39 +0800

    wip: cache

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,19 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             // parse url query
             let url = match req.url() {
                 Ok(url) => url,
-                Err(_) => return Response::error("failed to parse url", 400),
+                Err(_) => return Response::error("failed to parse url", 404),
             };
+
+            // check if cache exists
+            let c = Cache::default();
+            let key = url.to_string();
+            let cached_response = match c.get(&key, true).await {
+                Ok(cached_response) => cached_response,
+                Err(_) => None,
+            };
+            if let Some(cached_response) = cached_response {
+                return Ok(cached_response);
+            }
 
             let qs = url
                 .query()
@@ -43,7 +54,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             // extract url, width, and height
             let url = match qs.get("url") {
                 Some(url) => url.to_owned(),
-                None => return Response::error("url could not be found", 400),
+                None => return Response::error("url could not be found", 404),
             };
             // defaults both w and h to 256
             let width = qs
@@ -64,28 +75,28 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             // let's fetch the image
             let req = match Request::new(url, worker::Method::Get) {
                 Ok(req) => req,
-                Err(_) => return Response::error("failed to create request", 400),
+                Err(_) => return Response::error("failed to create request", 404),
             };
             // fetch that shit!
             let f_u = Fetch::Request(req);
             let mut resp = match f_u.send().await {
                 Ok(resp) => resp,
-                Err(_) => return Response::error("failed to fetch image", 400),
+                Err(_) => return Response::error("failed to fetch image", 404),
             };
             let image_bytes = match resp.bytes().await {
                 Ok(image_bytes) => image_bytes,
-                Err(_) => return Response::error("failed to process image", 400),
+                Err(_) => return Response::error("failed to process image", 404),
             };
 
             // create image from response
             let cursor_bytes = Cursor::new(&image_bytes);
             let img_reader = match ImageReader::new(cursor_bytes).with_guessed_format() {
                 Ok(img) => img,
-                Err(_) => return Response::error("failed to process image", 400),
+                Err(_) => return Response::error("failed to process image", 404),
             };
             let img = match img_reader.decode() {
                 Ok(img) => img,
-                Err(_) => return Response::error("failed to process image", 400),
+                Err(_) => return Response::error("failed to process image", 404),
             };
             let resized_img = img.resize_to_fill(width, height, FilterType::Lanczos3);
 
@@ -93,22 +104,27 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             let mut buf = Cursor::new(Vec::new());
             match resized_img.write_to(&mut buf, image::ImageOutputFormat::Jpeg(90)) {
                 Ok(_) => (),
-                Err(_) => return Response::error("failed to process image", 400),
+                Err(_) => return Response::error("failed to process image", 404),
             };
 
             // construct new response
             let resp = match Response::from_bytes(buf.into_inner()) {
                 Ok(resp) => resp,
-                Err(_) => return Response::error("failed to generate response", 400),
+                Err(_) => return Response::error("failed to generate response", 404),
             };
             // with the following headers
             let mut headers = worker::Headers::new();
             match headers.set("Content-Type", "image/jpeg") {
                 Ok(_) => (),
-                Err(_) => return Response::error("failed to generate response", 400),
+                Err(_) => return Response::error("failed to generate response", 404),
             }
-            // then, push mo yan teh!
-            Ok(resp.with_status(200).with_headers(headers))
+            // cache control headers
+            match headers.set("Cache-Control", "public, max-age=31536000") {
+                Ok(_) => (),
+                Err(_) => return Response::error("failed to generate response", 404),
+            }
+            let resp = resp.with_status(200).with_headers(headers);
+            Ok(resp)
         })
         .run(req, env)
         .await
